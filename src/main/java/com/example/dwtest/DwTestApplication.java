c803eded6dee47a14a724a5cd41a76d16f7837f6
chacha86 <taejincha0619@gmail.com>
2024-05-20 17:00:35 +0900

    init

---
+++ src/main/java/com/example/dwtest/DwTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.dwtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dwtest/DwTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dwtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
